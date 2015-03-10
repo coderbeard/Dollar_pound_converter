c0a06a3f9a9277cdca1b6c879b460bb330fc5ce8
coderbeard <melbeik02@gmail.com>
2015-03-10 14:00:39 +0000

    added somethng

--- Converter.py
+++ Converter.py
@@ -1,1 +1,15 @@
-#test
+class converter:
+    def __init__(self):
+        self.filename = "conversions.txt"
+        self.dict = {}
+        
+    def add_currency(self):
+        action = input("Hi, type in a country and the country that you would like to convert into to.")
+        print("structure it in the format of,  your country - other country: conversion rate")
+        for i in open(self.filename, "r+"):
+            segments = i.strip(":", 1)
+            self.map[segments[0] = segments[1].rstrip("\n")
+            
+            
+    
+c1 = converter()
