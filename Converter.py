ec2f2ea39dc2684c6d24f1b1d5c42dd3898472d6
coderbeard <melbeik02@gmail.com>
2015-03-11 13:45:19 +0000

    starting section of my code. Asks you for the conversion rate

--- Converter.py
+++ Converter.py
@@ -1,14 +1,18 @@
 class converter:
     def __init__(self):
         self.filename = "conversions.txt"
-        self.dict = {}
+        self.diction = {}
+        file = self.filename
+        
         
     def add_currency(self):
-        action = input("Hi, type in a country and the country that you would like to convert into to.")
-        print("structure it in the format of,  your country - other country: conversion rate")
+        print()
+        print("Hi, type in a country and the country that you would like to convert into to.")
+        action = input("structure it in the format of,  your country - other country: conversion rate")
         for i in open(self.filename, "r+"):
-            segments = i.strip(":", 1)
-            self.map[segments[0] = segments[1].rstrip("\n")
+            segments = i.rstrip(":", 1)
+            self.diction[segments[0]] = segments[0].rstrip("\n")
+            
             
             
     
